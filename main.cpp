1619bfd46324f578595f95ab84ce0765a986f6fc
Igor Makarevich <igor.makarevich@novometgroup.com>
2021-06-21 17:45:38 +0500

    -fixed "magic number" problem; -fixed double Petya winning problem.

--- main.cpp
+++ main.cpp
@@ -35,12 +35,13 @@ char get_char (std::string line, int x, int y) {
 
 int sum_char (std::string line, char ch) {
     int sum = 0;
+    int error = 666;
     for (int y = 1; y <= 3; ++y) {
         for (int x = 1; x <= 3; ++x) {
             if (get_char(line, x, y) == ch) {
                 ++sum;
             } else if (get_char(line, x, y) != 46 && get_char(line, x, y) != 79 && get_char(line, x, y) != 88) {
-                return 666;
+                return error;
             }
         }
     }
@@ -72,7 +73,7 @@ std::string check_winer (std::string line1, std::string line2, std::string line3
     petyaW += chek_line(dLine2, 'X');
     vanyaW += chek_line(dLine1, 'O');
     vanyaW += chek_line(dLine2, 'O');
-    if (petyaW == 1 && vanyaW == 0) {
+    if ((petyaW == 1 || petyaW == 2) && vanyaW == 0) {
         result = "Petya won.";
     } else if (vanyaW == 1 && petyaW == 0) {
         result = "Vanya won.";
@@ -86,10 +87,11 @@ std::string check_winer (std::string line1, std::string line2, std::string line3
 
 bool chek_input (std::string one, std::string two, std::string three) {
     std::string full = one + two + three;
+    int error = 666;
     int l = full.length() - 1;
     int charX = 0, charO = 0, point = 0;
     if (one.length() != 3 || two.length() != 3 || three.length() != 3
-        || sum_char(full, 'X') == 666
+        || sum_char(full, 'X') == error
         || sum_char(full, 'O') > sum_char(full, 'X')) {
         return false;
     }
